6d13870da95c2f2cfd31861932abc58aa9a6ad64
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-24 19:35:50 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Renderer renderer = new Renderer();
+            Player player = new Player(5,5);
+            renderer.ShowPosition(player._xPosition,player._yPosition);
+        }
+    }
+
+    class Player
+    {
+        public int _xPosition { get; private set; }
+        public int _yPosition { get; private set; }
+
+        public Player(int xPosition, int yPosition)
+        {
+            _xPosition = xPosition;
+            _yPosition = yPosition;
+        }
+    }
+    class Renderer
+    {
+        public void ShowPosition(int xPosition, int yPosition, char character = '*')
+        {
+            Console.SetCursorPosition(xPosition, yPosition);
+            Console.Write(character);
+        }
+    }
+}
